3eb0dc6885a1fb0b958e2833209199dff698927a
Lucas Johann <gabif.johann@gmail.com>
2023-11-23 16:34:14 -0300

    add: zelda service package structure

---
+++ zelda_service/src/test/java/com/github/api_projeto_final/zelda_service/ZeldaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.api_projeto_final.zelda_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeldaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
