d4485e0bd4804286582cdc1a653090864ac6faa0
Lucas Johann <gabif.johann@gmail.com>
2023-11-23 16:34:14 -0300

    add: zelda service package structure

---
+++ zelda_service/src/main/java/com/github/api_projeto_final/zelda_service/ZeldaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.api_projeto_final.zelda_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeldaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeldaServiceApplication.class, args);
+	}
+
+}
