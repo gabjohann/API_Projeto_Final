4bd6cda9d85e3a5d5affe98916ded03b1f0532d9
davi <davikraieski@gmail.com>
2023-11-23 21:23:31 -0300

    add: user-service

---
+++ user-service/src/main/java/com/github/api_projeto_final/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.api_projeto_final.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
