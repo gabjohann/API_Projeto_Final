3e775b9d45ff4f1c31317701c5ac5d4ae703ea19
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2023-11-30 16:24:48 -0300

    add:gateway-service

---
+++ gateway_service/src/main/java/com/github/api_projeto_final/gateway_service/GatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.api_projeto_final.gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
---
+++ gateway_service/src/test/java/com/github/api_projeto_final/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.api_projeto_final.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
