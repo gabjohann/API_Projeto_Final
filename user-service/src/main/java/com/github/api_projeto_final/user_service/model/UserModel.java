fcbdcbf258215db50ede1b19781c2315a14e9610
Lucas Johann <gabif.johann@gmail.com>
2023-12-05 16:11:54 -0300

    add: user model

--- user-service/src/main/java/com/github/api_projeto_final/user_service/model/UserModel.java
+++ user-service/src/main/java/com/github/api_projeto_final/user_service/model/UserModel.java
@@ -1,23 +1,23 @@
 package com.github.api_projeto_final.user_service.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @Entity
 @Table(name = "users", schema = "zelda_database")
 public class UserModel {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id_user;
-
+    @NotBlank
     private String name;
-
+    @NotBlank
     private int age;
 
+    public UserModel(@NotBlank(message = "O nome do usuário é obrigatório!") String name, @NotEmpty(message = "A idade do usuário é obrigatória!") int age) {}
 }
